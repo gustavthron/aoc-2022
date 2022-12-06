09aaac24188930e6c00f7f2ac415ffd3a165110c
gustavthron <gustavthron@outlook.com>
2022-12-06 10:06:26 +0100

    Add day06 solution

---
+++ day06/Program.cs
@@ -0,0 +1,16 @@
+﻿int SolvePuzzle(int distinctCount)
+{
+    var input = File.ReadAllText("input.txt").ToCharArray();
+    var received = new List<char>();
+    for (int i = 0; i < input.Length; i++)
+    {
+        received.Add(input[i]);
+        if (received.TakeLast(distinctCount).Distinct().Count() == distinctCount)
+        {
+            return i + 1;
+        }
+    }
+    return 0;
+}
+
+Console.WriteLine(SolvePuzzle(Environment.GetEnvironmentVariable("part") == "part1" ? 4 : 14));
